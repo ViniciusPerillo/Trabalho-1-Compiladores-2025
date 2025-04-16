215a06772f8601e798a5a37ae45ffd4c1aa64953
Vinicius Perillo <vinicius.perillo25@gmail.com>
2025-04-16 00:28:42 -0300

    exercicios 1 a 14 funcionando

--- Jander.g4
+++ Jander.g4
@@ -1,15 +1,25 @@
 lexer grammar Jander;
 WS : [ \t\n\r]+ -> skip ;
 COMMENT : '{'.*?'}' -> skip;
-PRES : 'algoritmo' | 'fim_algoritmo' | 'declare' | 'leia' | 'escreva' | 'inteiro' | 'literal';
-VARIAVEL : ('a'..'z' | 'A'..'Z') ('a'..'z' | 'A'..'Z' | '0'..'9')*;
-NUMINT : ('+'|'-')?('0'..'9')+;
-NUMREAL : ('+'|'-')?('0'..'9')+ ('.' ('0'..'9')+)?;
-STRING : '"'.*?'"';
-OP_REL : '>' | '>=' | '<' | '<=' | '<>' | '<-';
+P_RES : 'algoritmo' | 'fim_algoritmo' | 'declare' | 'leia' | 'escreva' | 'inteiro' | 'literal' | 'real' | 'logico' | 'se' | 'entao' | 'fim_se' | 'caso' |'senao' | 'fim_caso' | 'para' | 'faca' | 'fim_para' | 'enquanto' | 'fim_enquanto' | 'ate';
+OP_LOG : 'e' | 'ou' | 'nao';
+
+IDENT : ('a'..'z' | 'A'..'Z') ('a'..'z' | 'A'..'Z' | '0'..'9')*;
+NUM_INT : ('+'|'-')?('0'..'9')+;
+NUM_REAL : ('+'|'-')?('0'..'9')+ ('.' ('0'..'9')+)?;
+CADEIA : '"'.*?'"';
+
+OP_REL : '>' | '>=' | '<' | '<=' | '<>' | '<-' | '=';
 OP_ARIT : '+' | '-' | '*' | '/';
+
 DELIM : ':';
 ABREPAR : '(';
 FECHAPAR: ')';
 VIRGULA: ',';
+INTERVALO: '..'; 
+PONTEIRO: '^';
+ENDERECO: '&';
+
+
+
 
--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 import sys
+import os
 from antlr4 import *
 from Jander import Jander
 
 def main(argv):
     input_stream = FileStream(argv[1],encoding="utf-8")
+    output = argv[2]
     lexer = Jander(input_stream)
     token = lexer.nextToken()
 
-    while token.type != Token.EOF:
-        nome_token = Jander.symbolicNames[token.type]
-        if nome_token in ['VARIAVEL', 'NUMINT', 'NUMREAL', 'PRES', 'STRING']:
-            print(f"<{nome_token}, '{token.text}'>")
-        else:
-            print(f"<{nome_token}, '{token.text}'>")
+    with open(output, mode='w') as out:
+        while token.type != Token.EOF:
+            nome_token = Jander.symbolicNames[token.type]
+        
+            if nome_token in ['IDENT', 'NUM_INT', 'NUM_REAL', 'CADEIA']:
+                print(f"<'{token.text}',{nome_token}>", file= out)
+            else:
+                print(f"<'{token.text}','{token.text}'>", file= out)
 
-        token = lexer.nextToken()
+            token = lexer.nextToken()
+
+def tester(argv):
+    output = argv[2]
+    saida_correta = argv[1].split('entrada')
+    with open(output, mode='r') as f:
+        out_lines = f.readlines()
+
+    with open('saida'.join(saida_correta), mode= 'r') as f:
+        corr = f.readlines()
+
+    for idx in range(len(corr)):
+        if corr[idx] != out_lines[idx]:
+            print(f'{idx}: linha deveria ser {corr[idx]} ao inves de {out_lines[idx]}')
+        
+
+# if __name__ == '__main__':
+#     main(sys.argv)
+#     tester(sys.argv)
+
+
+def run():
+    root = 'D:\\gitRep\\Trabalho-1-Compiladores-2025\\casos_de_teste\\entrada\\'
+    for case in os.listdir(root):
+        args = [None, root + case, 'output.txt']
+        print(case)
+        print('\n\n')
+        main(args)
+        tester(args)
+        print('\n\n\n\n\n')
 
 if __name__ == '__main__':
-    main(sys.argv)
+    run()
